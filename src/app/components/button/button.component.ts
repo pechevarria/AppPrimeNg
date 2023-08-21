ddb6c459cb551f7029a8407ebd49995361bc1ee3
pechevarria <pechevarria05@gmail.com>
2023-08-21 01:00:13 -0500

    Commit Inicial de Proyecto base PrimeNG

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent {}
